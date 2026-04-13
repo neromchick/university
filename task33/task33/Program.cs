f60050cbe89ac7a7df7bd3f72ae7cf33e7730bb9
neromchick <155348639+neromchick@users.noreply.github.com>
2026-04-13 18:53:23 +0300

    Refactor LCM endpoint to use query parameters
    Refactor the endpoint to accept query parameters 'x' and 'y' and validate them before calculating LCM.

--- task33/task33/Program.cs
+++ task33/task33/Program.cs
@@ -1,17 +1,21 @@
 using task33;
 
 var builder = WebApplication.CreateBuilder(args);
-
 var app = builder.Build();
 
 app.MapGet("/romamelchenko64_gmail_com", async context =>
 {
-    var response = context.Response;
-    long[] parameters = new long[2]; int i = 0;
-    foreach (var item in context.Request.Query)
-        parameters[i++] = Convert.ToInt64(item.Value);
+    var x = context.Request.Query["x"].ToString();
+    var y = context.Request.Query["y"].ToString();
+
+    if (!long.TryParse(x, out long xVal) || !long.TryParse(y, out long yVal) || xVal < 0 || yVal < 0)
+    {
+        await context.Response.WriteAsync("NaN");
+        return;
+    }
+
     Calculator calc = new Calculator();
-    await response.WriteAsync(calc.GetLcm(parameters[0], parameters[1]).ToString());
+    await context.Response.WriteAsync(calc.GetLcm(xVal, yVal).ToString());
 });
 
 app.Run();
