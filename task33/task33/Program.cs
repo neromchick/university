91cceb43df91a5a9426534be383739f32b95bb80
neromchick <155348639+neromchick@users.noreply.github.com>
2026-04-13 19:04:05 +0300

    Update Program.cs

--- task33/task33/Program.cs
+++ task33/task33/Program.cs
@@ -1,4 +1,5 @@
 using task33;
+using System.Numerics;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
@@ -8,14 +9,18 @@ app.MapGet("/romamelchenko64_gmail_com", async context =>
     var x = context.Request.Query["x"].ToString();
     var y = context.Request.Query["y"].ToString();
 
-    if (!long.TryParse(x, out long xVal) || !long.TryParse(y, out long yVal) || xVal < 0 || yVal < 0)
+    if (!BigInteger.TryParse(x, out BigInteger xVal) || !BigInteger.TryParse(y, out BigInteger yVal) || xVal < 0 || yVal < 0)
     {
+        context.Response.ContentType = "text/plain";
         await context.Response.WriteAsync("NaN");
         return;
     }
 
     Calculator calc = new Calculator();
-    await context.Response.WriteAsync(calc.GetLcm(xVal, yVal).ToString());
+    var result = calc.GetLcm(xVal, yVal);
+
+    context.Response.ContentType = "text/plain";
+    await context.Response.WriteAsync(result.ToString());
 });
 
 app.Run();
